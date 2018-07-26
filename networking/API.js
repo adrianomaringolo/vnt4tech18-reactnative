855cd13c86864816544ad3d290befc4a87956081
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2018-07-26 14:52:22 -0300

    Usando AXIOS

---
+++ networking/API.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://vjobs.herokuapp.com';
+
+const getJobs = () => {
+  return axios.get('/jobs')
+    .then(response => response.data);
+}
+
+module.exports = {
+  getJobs
+}
